d304bfb0f42cbb9a3d064b687123c0414f1fafb2
Michael COLACINO <mcolacino@mac.com>
2018-12-09 09:24:09 -0500

    Working version (displays plot; also error msgs)

---
+++ bayesReg2.py
@@ -0,0 +1,61 @@
+# 181206 created bayesReg2.py
+# derived from https://www.quantstart.com/articles/Bayesian-Linear-Regression-Models-with-PyMC3
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+sns.set(style="darkgrid", palette="muted")
+
+
+def simulate_linear_data(N, beta_0, beta_1, eps_sigma_sq):
+    """
+    Simulate a random dataset using a noisy
+    linear process.
+
+    N: Number of data points to simulate
+    beta_0: Intercept
+    beta_1: Slope of univariate predictor, X
+    """
+    # Create a pandas DataFrame with column 'x' containing
+    # N uniformly sampled values between 0.0 and 1.0
+    df = pd.DataFrame(
+        {"x": 
+            np.random.RandomState(42).choice(
+                list(map(
+                    lambda x: float(x)/100.0, 
+                    np.arange(100)
+                )), N, replace=False
+            )
+        }
+    )
+
+    # Use a linear model (y ~ beta_0 + beta_1*x + epsilon) to 
+    # generate a column 'y' of responses based on 'x'
+    eps_mean = 0.0
+    df["y"] = beta_0 + beta_1*df["x"] + np.random.RandomState(42).normal(
+        eps_mean, eps_sigma_sq, N
+    )
+
+    return df
+
+
+if __name__ == "__main__":
+    # These are our "true" parameters
+    beta_0 = 1.0  # Intercept
+    beta_1 = 2.0  # Slope
+
+    # Simulate 100 data points, with a variance of 0.5
+    N = 100
+    eps_sigma_sq = 0.5
+
+    # Simulate the "linear" data using the above parameters
+    df = simulate_linear_data(N, beta_0, beta_1, eps_sigma_sq)
+
+    # Plot the data, and a frequentist linear regression fit
+    # using the seaborn package
+    sns.lmplot(x="x", y="y", data=df, size=10)
+    plt.xlim(0.0, 1.0)
+    plt.show()
